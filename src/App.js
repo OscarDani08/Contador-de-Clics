0f4ebb3e685edfc3a8d5c2926605a410fb916166
Oscar Quiñones <oscardqq@hotmail.com>
2023-09-21 20:08:01 -0500

    Generamos aplicacion para contar los clics que de el usuario, hicimos usos de los componentes estados y hooks con react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './Componentes/boton';
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png";
+import Contador from './Componentes/contador';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setNumclics] = useState(0)
+
+  const manejarClic = () => {
+    setNumclics (numClics + 1)
+  };
+
+  const reiniciarContador = () => {
+    setNumclics (0)
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-logo'>
+        <img
+        className='logo'
+        src={freeCodeCampLogo}
+        alt='Logo de FreecodeCamp'/>
+      </div>
+
+      <div className='contenedor-principal'>
+        <Contador
+        numClics= { numClics } />
+        <Boton
+          texto='Clic'
+          esBotonClic={true}
+          manejarClic={manejarClic}/>
+          
+        <Boton
+          texto='Reinciar'
+          esBotonClic={false}
+          manejarClic={reiniciarContador}/>
+      </div>
     </div>
   );
 }
